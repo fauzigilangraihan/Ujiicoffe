0058b0d50de56e3096ee513190c3a55b5e88546b
Fauzi Gilang Raihan <fauzigilangr10@gmail.com>
2025-05-03 14:09:14 +0700

    Ujiicoffe

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.getElementById('burger-menu');
-    const navbarNav = document.querySelector('.navbar-nav');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const burgerMenu = document.getElementById('burger-menu');
+//     const navbarNav = document.querySelector('.navbar-nav');
 
-    burgerMenu.addEventListener('click', function (e) {
-        e.preventDefault();
-        navbarNav.classList.toggle('active');
-    });
+//     burgerMenu.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         navbarNav.classList.toggle('active');
+//     });
+// });
+
+const navbarNav = document.querySelector
+('.navbar-nav');
+
+document.querySelector('#burger-menu').onclick = () =>{
+    navbarNav.classList.toggle('active');
+};
+
+const hamburger = document.querySelector('#burger-menu');
+
+document.addEventListener('click',function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
 });
