9ec0eb77e86e2502e4821994b84deb993b29cc6e
fauzigilangraihan <fauzigilangr10@gmail.com>
2025-05-03 14:19:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const burgerMenu = document.getElementById('burger-menu');
-//     const navbarNav = document.querySelector('.navbar-nav');
-
-//     burgerMenu.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         navbarNav.classList.toggle('active');
-//     });
-// });
-
 const navbarNav = document.querySelector
 ('.navbar-nav');
 
